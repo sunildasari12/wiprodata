bcbe94d1ca1f207a13815f93fcc65eb7dc1feb0d
sunildasari12 <dasarisunil9359@gmail.com>
2025-08-02 16:31:32 +0530

    code

---
+++ Day10/LspWith/Mounika.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LspWith
+{
+    internal class Mounika : Details
+    {
+        public override void ShowInfo()
+        {
+            Console.WriteLine("Hi I am Mounika...");
+        }
+    }
+}
