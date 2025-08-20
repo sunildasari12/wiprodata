5e5e85b12cc9581ab4c0c528dda5b408834453ea
sunildasari12 <dasarisunil9359@gmail.com>
2025-08-20 08:28:07 +0530

    codes

---
+++ Aug11/typescript/prog9.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var EmployNew_1 = require("./EmployNew");
+var obj1 = new EmployNew_1.Employ(1, "Pralavi", 84823);
+console.log("Employ No ".concat(obj1.empno, "  Employ Name ").concat(obj1.name, " \n                Salary ").concat(obj1.basic));
---
+++ Aug11/typescript/prog9.ts
@@ -0,0 +1,4 @@
+import { Employ } from "./EmployNew";
+const obj1 = new Employ(1, "Pralavi",84823);
+console.log(`Employ No ${obj1.empno}  Employ Name ${obj1.name} 
+                Salary ${obj1.basic}`)
